a17b90e442b5e78e1a05e17db0ec68ddee9e44cf
Imran Hussain <imranhussain399@gmail.com>
2023-05-23 17:20:52 +0500

    Add button for the user to set grid size. Also modified app.js file to generate random background color for the div element on mouse hover.

--- app.js
+++ app.js
@@ -1,23 +1,58 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const divContainer = document.createElement("div");
-  divContainer.classList.add("div-container");
-  document.body.appendChild(divContainer);
-  const getDiv = function (gridSize = 16) {
-    if (gridSize > 100) {
-        getDiv(prompt(`You entered grid size ${gridSize} which is greater 
-        than maximum\n limit of 100! Please enter grid size less than 100`,''));
-    } 
-    else {
-      const totalDivs = gridSize ** 2;
-      const divWidth = 100 / gridSize - 0.1; // 0.1 subtracted to adjust gap between squares
-      const divHeight = 100 / gridSize - 0.1;
-      for (let i = 1; i <= totalDivs; i++) {
-        let div = document.createElement("div");
-        div.style.minWidth = `${divWidth}vw`;
-        div.style.minHeight = `${divHeight}vh`;
-        divContainer.appendChild(div);
-      }
+document.addEventListener("DOMContentLoaded", ()=>{
+    const divContainer = document.createElement("div");
+    const btn = document.createElement("button");
+    btn.textContent = "Click to Change Grid Size";
+    divContainer.classList.add("div-container");
+    document.body.appendChild(divContainer);
+    document.body.prepend(btn);
+
+    const removeAllChilds =  function(parent){
+        while(parent.firstChild){
+            parent.removeChild(parent.firstChild)
+        }
     }
-  };
-  getDiv(prompt("Enter Grid Size: (max 100)", ""));
-});
+    const getGridSize = function(gridSize=16){
+        // First remove already existing childs 
+        removeAllChilds(divContainer);
+        if(gridSize>100){
+            getGridSize(prompt(`You entered grid size of ${gridSize} which is greater 
+than maximum limit of 100! Please enter grid size less than 100`,''));
+        }
+        else{
+            const totalDivs = gridSize**2;
+            const divWidth = 100/gridSize-0.0;
+            const divHeight = 100/gridSize-0.0;
+            for(let i=1; i<=totalDivs; i++){
+                let div = document.createElement("div");
+                div.classList.add("squareDiv")
+                div.style.minWidth = `${divWidth}vw`;
+                div.style.minHeight = `${divHeight}vh`;
+                divContainer.appendChild(div)
+            }
+        }
+    }
+
+    btn.addEventListener("click", ()=>{
+        getGridSize(prompt("Enter Grid Size(max 100)", ''));
+    })
+    getGridSize();
+    const colorGenerator = function(){
+        const colorLetters = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+    let colorCode = "";
+    for(let i=0; i<6; i++){
+        let randomNum = Math.floor(Math.random()*16);
+        colorCode+= colorLetters[randomNum];
+    }
+    return colorCode;
+    }
+    colorGenerator()
+    // Event delegation to the parent element
+    divContainer.addEventListener("mouseover", function(e){
+        let hoveredChild = e.target;
+        hoveredChild.style.backgroundColor = `#${colorGenerator()}`;
+    })
+    divContainer.addEventListener("mouseout", function(e){
+        let hoveredChild = e.target;
+        hoveredChild.style.backgroundColor = "#b8b9b8";
+    })  
+    });
